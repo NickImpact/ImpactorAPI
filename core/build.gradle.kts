a39b674991f62ea91dad98dd99cf0657e190958f
Nick <nick.degruccio@gmail.com>
2023-03-17 00:23:30 -0700

    Upgrade cloud to 1.7.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,13 +8,13 @@ dependencies {
     api("net.impactdev:json:1.0.0")
 
     // Adventure
-    api("net.kyori:adventure-api:4.11.0")
-    api("net.kyori:adventure-nbt:4.11.0")
-    api("net.kyori:adventure-text-serializer-plain:4.11.0")
-    api("net.kyori:adventure-text-serializer-legacy:4.11.0")
-    api("net.kyori:adventure-text-serializer-gson:4.11.0")
-    api("net.kyori:adventure-text-minimessage:4.11.0")
-    api("net.kyori:adventure-text-logger-slf4j:4.11.0")
+    api("net.kyori:adventure-api:4.13.0")
+    api("net.kyori:adventure-nbt:4.13.0")
+    api("net.kyori:adventure-text-serializer-plain:4.13.0")
+    api("net.kyori:adventure-text-serializer-legacy:4.13.0")
+    api("net.kyori:adventure-text-serializer-gson:4.13.0")
+    api("net.kyori:adventure-text-minimessage:4.13.0")
+    api("net.kyori:adventure-text-logger-slf4j:4.13.0")
 
     // Google
     api("com.google.guava:guava:31.1-jre")
@@ -28,13 +28,6 @@ dependencies {
     api("org.spongepowered:configurate-gson:4.1.2")
     api("org.spongepowered:configurate-hocon:4.1.2")
     api("org.spongepowered:configurate-yaml:4.1.2")
-
-    // Databases
-    api("com.zaxxer:HikariCP:4.0.3")
-    api("com.h2database:h2:2.1.214")
-    api("mysql:mysql-connector-java:8.0.28")
-    api("org.mariadb.jdbc:mariadb-java-client:2.7.2")
-    api("org.mongodb:mongo-java-driver:3.12.2")
 
     // Misc
     api("io.leangen.geantyref:geantyref:1.3.11")
